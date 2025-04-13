f6896b922b9db38837d12101d287c2ee34c30067
jameselite <soroushdeveloper20@gmail.com>
2025-04-13 16:24:12 +0330

    i forgot to load the content on the memory and then decode it

--- start/ParseTOML.go
+++ start/ParseTOML.go
@@ -2,16 +2,23 @@ package start
 
 import (
 	"errors"
+	"os"
 
 	"github.com/BurntSushi/toml"
 )
 
 func ParseTOML() (*TomlContent, error) {
 
+	tomlFile, tomlReadErr := os.ReadFile("gasp.toml")
+
+	if tomlReadErr != nil {
+		return nil, errors.New(tomlReadErr.Error())
+	}
+
 	var parsedTOML TomlContent
 
-	if _, parseErr := toml.Decode("gasp.toml", &parsedTOML); parseErr != nil {
-		
+	if _, parseErr := toml.Decode(string(tomlFile), &parsedTOML); parseErr != nil {
+
 		return nil, errors.New(parseErr.Error())
 	}
 
